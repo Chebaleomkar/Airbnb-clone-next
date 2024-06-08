c6b1848f92ea66ef42808ecb5345fe8c34ca242b
Omkar <omkarchebale0@gmail.com>
2024-06-08 10:25:37 +0530

    Navigation UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import "./globals.css";
 import {Nunito} from 'next/font/google'
+import Navbar from "@/components/Navbar/Navbar";
 
 
 export const metadata: Metadata = {
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
