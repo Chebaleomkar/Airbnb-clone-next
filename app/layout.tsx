c217c7fdd6e155ff7bb541bf6fa9bf6541ce90a9
Omkar <omkarchebale0@gmail.com>
2024-06-08 11:48:20 +0530

    modal UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import "./globals.css";
 import {Nunito} from 'next/font/google'
 import Navbar from "@/components/Navbar/Navbar";
+import Modal from "@/components/modal/Modal";
 
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal secondaryActionLabel="action" actionLabel="submit"  isOpen title="Hello Modal" />
         <Navbar />
         {children}
         </body>
